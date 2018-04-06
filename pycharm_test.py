87c5c006d021732e2a303b9d1f9c8fb266cf9069
irsol <irinywka@gmail.com>
2018-04-06 12:14:28 +0200

    pyCharm test file

---
+++ pycharm_test.py
@@ -0,0 +1,22 @@
+print('Since this string is in quotes and tied to print function, python knows to "print" it on the screen')
+x = 9
+x = 15
+print(x)
+print(x+2)
+print(x**2)
+print(x/2)
+print(x//2)
+
+x = 'two '
+y = 'three'
+
+print(x+y)
+
+ln1 = 'I will start the line here... '
+ln2 = ' and finish the line here'
+ln3 = ln1+ln2
+
+print(ln3)
+print(ln3+ln3)
+
+print('"this is a test"')
