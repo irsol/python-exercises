c143f35e03b00a015d62a4e726918d8d7f5d74cc
irsol <irinywka@gmail.com>
2018-04-16 21:07:17 +0200

    Update check_duplicates function

--- count_unique_letter.py
+++ count_unique_letter.py
@@ -1,37 +1,41 @@
 
+
 # 1
-def count_unique_letter(string):
-    """
-    get rid off any spaces: (' ', '')
-    """
-    string = string.replace(' ', '')
+def check_duplicates(text):
+    """ Checks if text contains duplicates.
 
-    """
-    Buit-in structure: set is an unordered collection of unique element,
-    comparing the leangth of it if it's unique or not.
+    Parameters:
+        - text (str) - string to check
+
+    Returns:
+        - True/False - True if string contains duplicates and False otherwise
     """
 
-    return len(set(string)) == len(string)
-print(count_unique_letter('llll p mmkj'))
+    # get rid off any spaces: (' ', '')
+    text = text.replace(' ', '')
 
-""" I returns Fals, because set ('llll p mmkj') it's 9 elemets and unique
-    elements only 3 (pkj) in a strig 'llll p mmkj', does set of not unique
-    elements equal to unique len(string), No!"""
+    return len(set(text)) != len(text)
+
+print(check_duplicates('llll p mmkj'))
+print(check_duplicates('l c n'))
 
 
 # 2
-def count_unique_letter(string):
-    string = string.replace(' ', '')
-    letters = set()
 
-    for letter in string:
-        if letter in letters:
-            """ empty set, think about dd elements, first d is unique and
-                in a set, but second isn't unique, so it's False """
-            return False
-        """ this is how set() letters filled in """
-        else:
-            letters.add(letter)
-                return True
-print(count_unique_letter('b d a')) #True
-print(count_unique_letter('b dd a cccc')) #False
+# def count_unique_letters(string):
+#     string = string.replace(' ', '')
+#     letters = set()
+#
+#     for letter in string:
+#         if letter in letters:
+#             """ empty set, think about dd elements, first d is unique and
+#                 in a set, but second isn't unique, so it's False """
+#             return False
+#         """ this is how set() letters filled in """
+#         else:
+#             letters.add(letter)
+#             return True
+#
+# print(count_unique_letter('b d a')) #True
+# print(count_unique_letter('b dd a cccc')) #False
+
