42074053109705b831525adb9dbad8a456ac30e0
irsol <irinywka@gmail.com>
2018-04-17 10:41:59 +0200

    Add comments to the function.

--- count_unique_letter.py
+++ count_unique_letter.py
@@ -22,18 +22,26 @@ print(check_duplicates('l c n'))
 
 # 2
 
-def count_unique_letters(string):
-    string = string.replace(' ', '')
+def count_unique_letters(text):
+    """ Counts quantity of all letters in a string.
+
+    Parameters:
+        - text (str) - letters to count
+
+    Returns:
+        - Dictionary with key and value
+    """
+
+    text = text.replace(' ', '')
     letters = {}
 
-    for letter in string:
+    for letter in text:
         if letter not in letters:
             letters[letter] = 1
         else:
             letters[letter] += 1
 
     return letters
-
 
 print(count_unique_letters('b d a'))
 print(count_unique_letters('b dd a cccc'))
