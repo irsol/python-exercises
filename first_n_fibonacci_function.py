40e2480e22f2b40982f899d95c3013bad4b725ab
irsol <irinywka@gmail.com>
2018-11-17 13:15:33 +0100

    Updated fibonacci function.

--- first_n_fibonacci_function.py
+++ first_n_fibonacci_function.py
@@ -1,15 +1,28 @@
+#
+# def fibo(num):
+#     n1 = 0
+#     n2 = 1
+#     result = [n1, n2]
+#     print("Fibonacci sequence: ")
+#
+#     for n in range(2, num):
+#         n3 = n1 + n2
+#         result.append(n3)
+#         n1 = n2
+#         n2 = n3
+#     print(result)
+#
+# fibo(5)
+
+
 def fibo(num):
-    n1 = 0
-    n2 = 1
-    result = [0]
+    result = [0, 1]
     print("Fibonacci sequence: ")
 
     for n in range(2, num):
-        n3 = n1 + n2
-        result.append(n2)
-        n1 = n2
-        n2 = n3
+        n3 = 0 + 1
+        result.append(n3)
+
     print(result)
-    #return
 
 fibo(5)
