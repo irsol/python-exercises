b102ec7bd26358116e6401168aa8a05ef1ef43d1
irsol <irinywka@gmail.com>
2019-02-12 16:52:23 +0100

    Updated functions.

--- sequence_of_numbers.py
+++ sequence_of_numbers.py
@@ -1,7 +1,7 @@
 # The filter will return all items from the list values which return True
 #  when passed to the function checkit. checkit will check if the value is in
 # the set. Since all the numbers in the set come from the values list,
-# all of the orignal values in the list will return True.
+# all of the original values in the list will return True.
 
 
 values = [1, 2, 1, 3]
@@ -13,6 +13,7 @@ def checkit(num):
         return True
     else:
         return False
+
 
 for i in filter(checkit, values):
     print(i)
