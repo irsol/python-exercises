874a70d0e77dbca96ae3a28bbb80a6c36a59a1e8
irsol <irinywka@gmail.com>
2019-02-12 16:49:58 +0100

    Updated functions.

--- boolean_practice/break_continue.py
+++ boolean_practice/break_continue.py
@@ -4,24 +4,24 @@ text_line = 1
 file = open("data.txt", "r")
 for line in file:
     line = line.strip()
-    if line == "Earth":
+    if "Earth" in line:
+        print(f"Earth is at line {text_line}")
         break
     text_line = text_line + 1
-print(f"Earth is at line {text_line}")
 
 
 # Finds which line in a txt file contains string "Earth".
 # This function uses continue statement to include all lines and skip comment lines.
 
-entry_num = 1
+entry_num = 0
 file = open("data.txt", "r")
 for line in file:
     line = line.strip()
+    entry_num = entry_num + 1
+
     if line.startswith("#"):
         continue
-    if line == "Earth":
+
+    if "Earth" in line:
+        print(f"Number: {entry_num}")
         break
-    entry_num = entry_num + 1
-print(f"Number: {entry_num}")
-
-
