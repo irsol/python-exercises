a1fd87f6618aef50b3efdc0bbef4d71e4dc3a71c
irsol <irinywka@gmail.com>
2019-02-17 11:44:57 +0100

    Updated functions.

--- mypythonquiz1.py
+++ mypythonquiz1.py
@@ -1,48 +1,56 @@
- #1: what does the following code do?
+ # 1: what does the following code do?
+
+
 def a(b, c, d):
     pass
 # The 'def' statement defines a function.
 # The 'pass' statement is a null operation.
 
 
-#2: what is the output of the following code?
+# 2: what is the output of the following code?
+
+
 print(type([1, 2]))
 #  Lists are formed by placing a comma-separated
-#list of expressions in square brackets
+# list of expressions in square brackets
 
 
-#3: what gets printed?
+# 3: what gets printed?
+
+
 def f():
     pass
+
+
 print(type(f()))
 # The argument to the type() call is a return value of
 # a function call, which returns None
 
 
-#4: what should the below code print?
+# 4: what should the below code print?
 print(type(1J))
-#<class 'complex'> or j means complex number
+# <class 'complex'> or j means complex number
 # you can put ‘j’ or ‘J’ after a number to make it imaginary,
 # so you can write complex literals
 
-#5: what is the output of the following code?
+# 5: what is the output of the following code?
 print(type(lambda: None))
 # <class 'function'>
 # 'lambda arguments: expression' yields a function object
 
 
-#6: what is the output of the below program?
+# 6: what is the output of the below program?
 a = [1, 2, 3, None, (), [], ]
 print(len(a))
 # 6
 # The trailing comma in the list is ignored, the rest are legitimate values
 
-#7: what gets printed?
+# 7: what gets printed?
 print(type(1/2))
 # <class 'float'>
 #  division of an integer by another integer yields a float
 
-#8: What gets printed?
+# 8: What gets printed?
 d = lambda p: p * 2
 t = lambda p: p * 3
 x = 2
@@ -50,15 +58,14 @@ x = d(x)
 x = t(x)
 x = d(x)
 print(x)
-# 24
-# start with 2, multiply by 2, multiply by 3, multipy by 2
 
-#10: What gets printed?
+# 10: What gets printed?
 nums = set([1, 1, 2, 3, 3, 3, 4])
 print(len(nums))
 # nums is a set, so only unique values are retained.
 
-#11: What gets printed?
+# 11: What gets printed?
+
 x = True
 y = False
 z = False
@@ -70,7 +77,8 @@ else:
 # yes
 #  AND is higher precedence than OR in python and is evaluated first
 
-# #12: What gets printed?
+# 12: What gets printed?
+
 x = True
 y = False
 z = False
@@ -87,36 +95,39 @@ else:
 #  NOT has first precedence, then AND, then OR
 
 
-#14: What gets printed?
+# 14: What gets printed?
+
 counter = 1
 
 
-def doLotsOfStuff():
+def do_lots_of_stuff():
     global counter
-    for i in (1, 2, 3):
+    for num in (1, 2, 3):
         counter += 1
 
-doLotsOfStuff()
+
+do_lots_of_stuff()
 print(counter)
+
 # 4
 #  the counter variable being referenced in the function is the global
 # variable defined outside of the function.  Changes to the variable in
 # the function affect the original variable.
 
-#16: What gets printed?
+# 16: What gets printed?
 print("\x48\x49!")
 # HI!
 # \x is an escape sequence that means the following 2 digits ares
 # a hexadicmal number encoding a character.
 
-#17: What gets printed?
+# 17: What gets printed?
 print(0xA + 0xa)
 # 20
 # 0xA and 0xa are both hexadecimal integer literals representing the decimal
 # value 10.  Their sum is 20.
 
 
-#18: What gets printed?
+# 18: What gets printed?
 class Parent:
     def __init__(self, param):
         self.v1 = param
@@ -126,13 +137,14 @@ class Child(Parent):
     def __init__(self, param):
         self.v2 = param
 
+
 obj = Child(11)
 print(obj.v1 + " " + obj.v2)
 #  AttributeError: child instance has no attribute 'v1'.  self.v1 was never
 # created as a variable since the parent __init__ was not explicitly called.
 
 
-#19: What following python function will return?
+# 19: What following python function will return?
 def my_cool_func(a, b, c):
     if a > (b + c):
         return a
@@ -140,6 +152,7 @@ def my_cool_func(a, b, c):
         return b
     else:
         return c
+
 
 my_cool_func(5, 3, 2)
 my_cool_func(7, 4, 4)
@@ -149,7 +162,7 @@ my_cool_func(3, 5, 9)
 # 9 c
 
 
-#20
+# 20
 for i in range(2):
     print(i)
 
@@ -160,7 +173,7 @@ for i in range(4, 6):
 # The default beginning of a range is 0. The range will include the beginning
 # of the range and all numbers up to but not including the end of range(4,5)
 
-#21
+# 21
 lst = [3, 4, 7, 1, 1]
 result = 0
 for i in range(len(lst)):
@@ -168,17 +181,17 @@ for i in range(len(lst)):
 print(result)
 # 16
 
-#22 Tuples
+# 22 Tuples
 lst = [("a", 1), ("b", 2), ("c", 3)]
 for letter, number in lst:
     print('{"letter=number"}'.format(*lst))
 
-#23 Dictionary
+# 23 Dictionary
 d = {"cat": "no",
      "dog": "yes"}
 print("fox" in d)
 
-#24
+# 24
 a = 10
 b = 7
 if a < 7:
@@ -192,7 +205,7 @@ elif a > b:
 else:
     print("end")
 
-#25 String
+# 25 String
 g = "I'm cat"
 print(g.replace("cat", "fox"))
 
