c286e2c6cff6491c2d6cf66258ce46e49cc0ad8b
irsol <irinywka@gmail.com>
2019-02-17 11:26:54 +0100

    Added min and max functions, used lambda.

--- file_processing/numbers.py
+++ file_processing/numbers.py
@@ -1,4 +1,5 @@
 import csv
+# Find sum, average, min and max ages in csv file
 
 count = 0
 with open("numbers.csv", "r") as file:
@@ -6,13 +7,15 @@ with open("numbers.csv", "r") as file:
     total = 0
     min_value = []
     rows = []
+
     for row in csv.reader(file,delimiter=";"):
         if row == []:  # check if there an empty string
             continue
-        rows.append(row)
 
+        rows.append(row)
         total += int(row[1])
         count = count + 1
+
     min_value = min(rows, key=lambda item: int(item[1]))
     max_value = max(rows, key=lambda item: int(item[1]))
     average_age = total / count
