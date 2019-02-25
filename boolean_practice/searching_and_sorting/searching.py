3e61c5ce9514495ac775c9626848060c39893060
irsol <irinywka@gmail.com>
2019-02-25 16:10:32 +0100

    Added linear search algorithm using for loop.

--- boolean_practice/searching_and_sorting/searching.py
+++ boolean_practice/searching_and_sorting/searching.py
@@ -1,4 +1,6 @@
 # Basic Linear Search
+
+
 def linear_search(v, l):
     """Return the index of the first occurrence of v in list l or return len(l)
     if v is not in l"""
@@ -7,3 +9,26 @@ def linear_search(v, l):
     while i != len(l) and l[i] != v:
         i += 1
     return i
+
+
+print(linear_search(3, [1, 5, 6, 3]))
+
+
+# for loop Version of Linear Search
+
+def linear_search(v, l):
+    """Return the index of the first occurrence of v in list l or return len(l)
+    if v is not in l"""
+    i = 0
+    for value in l:
+        if value == v:
+            return i
+        i += 1
+    return len(l)
+
+
+print(linear_search(52, [12, 52, 26, 13]))
+
+
+
+
